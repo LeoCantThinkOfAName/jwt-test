720e270c9e3d06fe2dee4a06d9d199fc64bca328
leocantthinkofanamae <i7851@hotmail.com>
2019-09-29 20:10:46 +0800

    first commit

---
+++ server/src/MyContext.ts
@@ -0,0 +1,10 @@
+import { Response, Request } from "express";
+
+export interface MyContext {
+  req: Request;
+  res: Response;
+  payload?: {
+    userId: string;
+    tokenVersion: number;
+  };
+}
---
+++ server/src/isAuth.ts
@@ -0,0 +1,23 @@
+import { MiddlewareFn } from "type-graphql";
+import { MyContext } from "./MyContext";
+import { verify } from "jsonwebtoken";
+
+// bearer 123123sadoasoi
+
+export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
+  const authorization = context.req.headers["authorization"];
+
+  if (!authorization) {
+    throw new Error("Not authenticated");
+  }
+
+  try {
+    const token = authorization.split(" ")[1];
+    const payload = verify(token, process.env.ACCESS_TOKEN!);
+    context.payload = payload as any;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return next();
+};
